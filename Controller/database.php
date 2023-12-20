9f8c46c5629b844169291b60f71602cae271a56a
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2023-12-20 13:56:52 +0100

    Creating database Connection

---
+++ Controller/database.php
@@ -0,0 +1,26 @@
+<?php
+class Database
+{
+    private $dsn;
+    private $conn;
+
+    public function __construct($host, $username, $password, $db)
+    {
+        $this->dsn = "mysql:host=$host;dbname=$db";
+
+        try {
+            $this->conn = new PDO($this->dsn, $username, $password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // if ($this->conn) {
+            //     echo "Connected to the $db database successfully!";
+            // }
+        } catch (PDOException $e) {
+            echo "Error Connection: " . $e->getMessage();
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+}
---
+++ database/config.php
@@ -0,0 +1,4 @@
+<?php 
+require '../Controller/database.php';
+
+$database = new Database('localhost', 'root', '', 'oop-workshop');
